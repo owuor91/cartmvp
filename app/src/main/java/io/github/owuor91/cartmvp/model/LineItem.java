444a8969481947edcc1ae09ba7fb02fdce663d5e
John Owuor <jo@refunite.org>
2017-06-15 17:00:44 +0300

    update pojos

--- app/src/main/java/io/github/owuor91/cartmvp/model/LineItem.java
+++ app/src/main/java/io/github/owuor91/cartmvp/model/LineItem.java
@@ -4,5 +4,25 @@ package io.github.owuor91.cartmvp.model;
  * Created by johnowuor on 15/06/2017.
  */
 
-public class LineItem {
+public class LineItem extends Product{
+    private int quantity;
+
+    public LineItem(){}
+
+    public LineItem(Product product, int quantity) {
+        super(product);
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getSumPrice(){
+        return getSalePrice()*quantity;
+    }
 }
--- app/src/main/java/io/github/owuor91/cartmvp/model/Product.java
+++ app/src/main/java/io/github/owuor91/cartmvp/model/Product.java
@@ -5,4 +5,34 @@ package io.github.owuor91.cartmvp.model;
  */
 
 public class Product {
+    private long id;
+    private String productName;
+    private String description;
+    private double salePrice;
+
+    public Product() {
+    }
+
+    public Product(Product product) {
+        this.id = product.getId();
+        this.productName = product.getProductName();
+        this.description = product.getDescription();
+        this.salePrice = product.getSalePrice();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getSalePrice() {
+        return salePrice;
+    }
 }
